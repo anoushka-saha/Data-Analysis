1ba76854c8396b648dee84aded72bde25136da0f
anoushka1111 <saha.anoushka@gmail.com>
2024-07-11 20:25:35 -0400

    zip files

--- filesystem shell methods.py
+++ filesystem shell methods.py
@@ -7,13 +7,26 @@
 import os
 from os import path
 import shutil
+from shutil import make_archive
+from zipfile import ZipFile
 
-# Making a duplicate of an existing file
-if path.exists("textfile.txt"):
+if path.exists("textfile.txt.bak"):
 
     # Retreive path info from file
     src = path.realpath("textfile.txt")
 
     # Making backup copy
-    dst = src + ".bak"
-    shutil.copy(src, dst)
+    # dst = src + ".bak"
+    # shutil.copy(src, dst)
+
+    # Renaming original file
+    # os.rename("textfile.txt", "newfile.txt")
+
+    # ZIP archive
+    root_dir, tail = path.split(src)
+    shutil.make_archive("archive", "zip", root_dir)
+
+    # More detailed control over ZIP files
+    with ZipFile("testzip.zip", "w") as newzip:
+        newzip.write("newfile.txt")
+        newzip.write("textfile.txt.bak")
