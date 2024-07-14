27fc0e20e9c89a5c56386c920122b2909a886be1
anoushka1111 <saha.anoushka@gmail.com>
2024-07-13 20:41:34 -0400

    updates

--- files_start.py
+++ files_start.py
@@ -7,20 +7,20 @@
 
 def main():
 
-    # #open() - opening a file 
-    # #"w" - to be able to write to the file
-    # #"+" - tells python to create the file if it doesn't exist
-    # #myfile = open("textfile.txt", "w+") 
+    # open() - opening a file 
+    # "w" - to be able to write to the file
+    # "+" - tells python to create the file if it doesn't exist
+    # myfile = open("textfile.txt", "w+") 
 
-    # #Opening file for appending text to end
+    # Opening file for appending text to end
     # myfile = open("textfile.txt", "a+")
 
-    # #Writing lines of data to file
+    # Writing lines of data to file
     # for i in range(10):
     #     #myfile.write("This is some text\n")
     #     myfile.write("This is some new text\n")
 
-    # #Close the file when finished
+    # Close the file when finished
     # myfile.close()  
 
     # Opening file for read access
