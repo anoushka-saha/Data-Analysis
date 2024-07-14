d9b7bc30222ddecb898148a5d932c2433e4e13b1
anoushka1111 <saha.anoushka@gmail.com>
2024-07-13 20:39:45 -0400

    timedeltas

--- dates and times.py
+++ dates and times.py
@@ -4,7 +4,7 @@
 # July 13th, 2024
 
 # Importing necessary modules
-from datetime import date, time, datetime
+from datetime import date, time, datetime, timedelta
 
 # Getting today's date
 today = date.today()
@@ -40,3 +40,26 @@ print(today_dt.strftime("Locale time: %X"))
 # Time formatting
 print(today_dt.strftime("Current time: %I:%M:%S %p"))
 print(today_dt.strftime("24-hour time: %H:%M"))
+
+# Constructing a basic timedelta
+print(timedelta(days = 365, hours = 5, minutes = 1))
+
+# Print today's date one year from now
+print("One year from now it will be", str(today_dt + timedelta(days = 365)))
+
+# Timedelta with multiple arguments
+print("In two weeks and 3 days it will be: ", str(today_dt + timedelta(weeks = 2, days = 3)))
+
+# Calculate the date 1 week ago
+print("One week ago it was: ", str(today_dt - timedelta(weeks = 1)))
+
+# How many days until April Fools?
+td = date.today()
+afd = date(td.year, 4, 1)
+
+if afd < td:
+    print("April Fool's Day already went by: ", ((td - afd).days))
+    afd = afd.replace(year = td.year + 1)
+
+time_to_afd = afd - td
+print("It is", time_to_afd.days," days until the next April Fool's Day!")
